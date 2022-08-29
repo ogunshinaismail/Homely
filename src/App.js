a5bb900aacc70885ee3b6cb6567fa20033d2d76c
ismail <ismailogunshina@gmail.com>
2022-08-30 00:59:22 +0800

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react'
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Home from './Pages/Home';
+import About from './Pages/About';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/about-us' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
