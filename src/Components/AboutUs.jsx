fcf93ed1ba76788f3f92e5d61f4b915f6e4cc9e0
ismail <ismailogunshina@gmail.com>
2022-08-30 21:57:10 +0800

    responsiveness

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -4,13 +4,13 @@ import { AboutImg } from '../Assets'
 const AboutUs = () => {
   return (
     <>
-        <div className="w-10/12 mx-auto my-24 grid grid-cols-2 items-end">
+        <div className="w-11/12 mx-auto my-24 grid lg:grid-cols-2 lg:w-10/12 items-end">
             <div>
-                <h1 className='font-bold text-5xl'>About Us</h1>
-                <p className="w-11/12 leading-9 text-2xl mt-5">I make delicious healthy meals for busy people who want to enjoy home made food without the hassle of cooking. Why would you cook when you don't have to? The Hot Plate makes it easy to eat your favorite home cooked food anywhere you want. You deserve to eat great food, whether you're at home or on the go.</p>
+                <h1 className='font-bold text-4xl lg:text-5xl'>About us</h1>
+                <p className="text-xl lg:w-11/12 lg:leading-9 lg:text-2xl mt-5">I make delicious healthy meals for busy people who want to enjoy home made food without the hassle of cooking. Why would you cook when you don't have to? The Hot Plate makes it easy to eat your favorite home cooked food anywhere you want. You deserve to eat great food, whether you're at home or on the go.</p>
                 <button className='bg-primary-600 px-8 py-2 text-white rounded-md mt-5 text-lg'>See more</button>
             </div>
-            <div className='flex justify-center'>
+            <div className='w-8/12 mx-auto mt-8 flex justify-center'>
                 <img src={AboutImg} alt="" />
             </div>
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,75 @@
-import React from 'react'
-import { Logo, Cart } from '../Assets'
+import React, { useState } from 'react'
+import { Logo, Cart, Hamburger  } from '../Assets'
 
 const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <>
-        <nav className='bg-primary-50'>
-            <div className="w-10/12 mx-auto flex items-center justify-between py-3">
-                <img src={Logo} alt="" />
-                <div className="flex items-center gap-5">
-                    <div className="flex gap-5 items-center">
-                        <p>Home</p>
-                        <p>About us</p>
-                        <p>Our products</p>
-                        <p>Testimonials</p>
-                        <p>Contact Us</p>
-                    </div>
-                    <button className='bg-primary-500 flex items-center gap-2 py-1 px-5 rounded-lg outline-none'>
-                        <img src={Cart} alt="" />
-                        <span className="text-white">Cart</span>
-                        <div className="bg-white text-black font-bold py-1 px-3 rounded-full">3</div>
-                    </button>
-                </div>
-            </div>
-        </nav>
+      <nav className="fixed top-0 w-full flex flex-wrap items-center justify-between py-3 bg-primary-50">
+
+        <div className="container w-11/12 lg:w-10/12 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a className="text-sm font-bold leading-relaxed inline-block whitespace-nowrap uppercase text-white" href="#home">
+              <img src={Logo} alt="" />
+            </a>
+
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 w-[60px] border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <img src={Hamburger} alt="" />
+            </button>
+          </div>
+
+          <div className={"lg:flex flex-grow" + (navbarOpen ? " flex" : " hidden")} id="example-navbar-danger">
+
+            <ul className="flex flex-col items-start lg:flex-row lg:items-center gap-5 list-none lg:ml-auto">
+              <li className="nav-item">
+                <a className="text-black hover:opacity-75" href="#home">
+                  <span className="ml-2 font-medium">Home</span>
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="text-black hover:opacity-75" href="#home">
+                  <span className="ml-2 font-medium">Why choose us</span>
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="text-black hover:opacity-75" href="#home">
+                  <span className="ml-2 font-medium">Our dishes</span>
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="text-black hover:opacity-75" href="#home">
+                  <span className="ml-2 font-medium">About us</span>
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="text-black hover:opacity-75" href="#home">
+                  <span className="ml-2 font-medium">Testimonials</span>
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <button className='bg-primary-500 flex items-center gap-2 py-1 px-5 rounded-lg outline-none'>
+                    <img src={Cart} alt="" />
+                    <span className="text-white">Cart</span>
+                    <div className="bg-white text-black font-bold py-1 px-3 rounded-full">3</div>
+                </button>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Navbar
