63eed1eb96ac753d99d5267f5b05c6d0cf8e819f
ismail <ismailogunshina@gmail.com>
2022-08-30 23:10:55 +0800

    onChange

--- src/Components/Dishes.jsx
+++ src/Components/Dishes.jsx
@@ -1,39 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { YamEgg, Spaghetti, Beans } from '../Assets'
 
 const Dishes = () => {
+    const [quantity, setQuantity] = useState(1)
+
     const dishes = [
         {
+            "id": 1,
             "name": "Yam and egg sauce",
             "img": YamEgg,
             "price": 1500.00
         },
         {
+            "id": 2,
             "name": "Spag and pepper sauce",
             "img": Spaghetti,
-            "price": 1500.00
+            "price": 1200.00
         },
         {
+            "id": 3,
             "name": "Porridge beans",
             "img": Beans,
-            "price": 1500.00
+            "price": 1400.00
         },
         {
+            "id": 4,
             "name": "Yam and egg sauce",
             "img": YamEgg,
-            "price": 1500.00
+            "price": 1100.00
         },
         {
+            "id": 5,
             "name": "Spag and pepper sauce",
             "img": Spaghetti,
-            "price": 1500.00
+            "price": 1600.00
         },
         {
+            "id": 6,
             "name": "Porridge beans",
             "img": Beans,
-            "price": 1500.00
+            "price": 900.00
         },
     ]
+
   return (
     <>
         <div className="lg:w-10/12 mx-auto mt-24">
@@ -42,14 +51,18 @@ const Dishes = () => {
 
             <div className="grid lg:grid-cols-3 my-10">
                 {dishes.map( dish => (
-                    <div className="flex flex-col items-center gap-5 border border-b-orange-500 lg:border-primary-500 py-10 lg:p-10">
+                    <div className="flex flex-col items-center gap-5 border border-b-orange-500 lg:border-primary-500 py-10 lg:p-10" key={dish.id}>
                         <img src={dish.img} alt="" className='w-[250px]' />
                         <p className="font-semibold text-2xl">{dish.name}</p>
 
                         <div className="flex items-center gap-32 lg:gap-10">
                             <p className='font-medium text-2xl'>₦{dish.price}</p>
                             <div className="flex items-center">
-                                <input type="number" className='w-16 mr-[-10px] border border-primary-500 px-2 rounded-md' />
+                                <input 
+                                    type="number" 
+                                    className='w-16 mr-[-10px] border border-primary-500 px-2 rounded-md'
+                                    value={quantity}
+                                    onChange={e => setQuantity(e.target.value)} />
                                 <button className='bg-primary-600 px-8 py-2 text-white rounded-lg'> + Add</button>
                             </div>
                         </div>
